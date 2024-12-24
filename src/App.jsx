9eadd9cc2e1ee196504fa037ddcc57a3af78bda6
LeonID <lmatskevich79@gmail.com>
2024-12-25 00:11:25 +0300

    feat: add AppHeader

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,38 @@
+import { Layout } from 'antd';
+import AppHeader from './components/layout/AppHeader';
+
+const contentStyle = {
+  textAlign: 'center',
+  minHeight: 'calc(100vh - 7.5rem)',
+  color: '#fff',
+  backgroundColor: '#001529',
+};
+const siderStyle = {
+  textAlign: 'center',
+  lineHeight: '120px',
+  color: '#fff',
+  backgroundColor: '#1677ff',
+};
+const footerStyle = {
+  textAlign: 'center',
+  height: 60,
+  color: '#fff',
+  backgroundColor: '#4096ff',
+};
+
 export default function App() {
-  return <h1>React Crypto App</h1>
+  return (
+    <>
+      <Layout>
+        <AppHeader />
+        <Layout>
+          <Layout.Sider width='25%' style={siderStyle}>
+            Sider
+          </Layout.Sider>
+          <Layout.Content style={contentStyle}>Content</Layout.Content>
+        </Layout>
+        <Layout.Footer style={footerStyle}>Footer</Layout.Footer>
+      </Layout>
+    </>
+  );
 }
