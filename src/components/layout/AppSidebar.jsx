f7b41e378d88c3b32b0de2adb758b1ff72eb1387
LeonID <lmatskevich79@gmail.com>
2024-12-29 23:45:06 +0300

    feat: add card description

--- src/components/layout/AppSidebar.jsx
+++ src/components/layout/AppSidebar.jsx
@@ -1,22 +1,12 @@
 import { ArrowDownOutlined, ArrowUpOutlined } from '@ant-design/icons';
-import { Card, Layout, List, Spin, Statistic, Typography } from 'antd';
+import { Card, Layout, List, Spin, Statistic, Typography, Tag } from 'antd';
 import React, { useEffect, useState } from 'react';
+import { percentDifference } from '../../utils';
 import { fetchAssets, fetchCrypto } from './api';
-import {percentDifference} from '../../utils'
 
 const sidebarStyle = {
   padding: '1rem',
 };
-
-const data = [
-  'Racing car sprays burning fuel into crowd.',
-  'Japanese princess to wed commoner.',
-  'Australian walks 100km after outback crash.',
-  'Man charged over missing wedding girl.',
-  'Los Angeles battles huge wildfires.',
-];
-
-
 
 export default function AppSidebar() {
   const [loading, setLoading] = useState(false);
@@ -53,39 +43,50 @@ export default function AppSidebar() {
 
   return (
     <Layout.Sider width='25%' style={sidebarStyle}>
-      <Card style={{ marginBottom: '1rem' }}>
-        <Statistic
-          title='Active'
-          value={11.28}
-          precision={2}
-          valueStyle={{
-            color: '#3f8600',
-          }}
-          prefix={<ArrowUpOutlined />}
-          suffix='%'
-        />
-        <List
-          size='small'
-          dataSource={data}
-          renderItem={item => (
-            <List.Item>
-              <Typography.Text mark>[ITEM]</Typography.Text> {item}
-            </List.Item>
-          )}
-        />
-      </Card>
-      <Card>
-        <Statistic
-          title='Idle'
-          value={9.3}
-          precision={2}
-          valueStyle={{
-            color: '#cf1322',
-          }}
-          prefix={<ArrowDownOutlined />}
-          suffix='%'
-        />
-      </Card>
+      {assets.map(asset => (
+        <Card key={asset.id} style={{ marginBottom: '1rem' }}>
+          <Statistic
+            title={asset.id}
+            value={asset.totalAmount}
+            precision={2}
+            valueStyle={{
+              color: asset.grow ? '#3f8600' : '#cf1322',
+            }}
+            prefix={asset.grow ? <ArrowUpOutlined /> : <ArrowDownOutlined />}
+            suffix='$'
+          />
+          <List
+            size='small'
+            dataSource={[
+              {
+                title: 'Total Profit:',
+                value: asset.totalProfit,
+                withTag: true,
+              },
+              { title: 'Total Amount:', value: asset.amount, isPlain: true },
+              // { title: 'Difference:', value: asset.growPercent },
+            ]}
+            renderItem={item => (
+              <List.Item>
+                <span>{item.title}</span>
+                <span>
+                  {item.withTag && (
+                    <Tag color={asset.grow ? 'green' : 'red'}>
+                      {asset.growPercent}%
+                    </Tag>
+                  )}
+                  {item.isPlain && item.value}
+                  {!item.isPlain && (
+                    <Typography.Text type={asset.grow ? 'success' : 'danger'}>
+                      {item.value.toFixed(2)}$
+                    </Typography.Text>
+                  )}
+                </span>
+              </List.Item>
+            )}
+          />
+        </Card>
+      ))}
     </Layout.Sider>
   );
 }
