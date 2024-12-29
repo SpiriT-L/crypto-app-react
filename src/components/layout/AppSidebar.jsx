76f277e723f2dc1cc015b7fbb4522d3390662df1
LeonID <lmatskevich79@gmail.com>
2024-12-29 22:55:25 +0300

    feat: add calculations

--- src/components/layout/AppSidebar.jsx
+++ src/components/layout/AppSidebar.jsx
@@ -15,6 +15,10 @@ const data = [
   'Los Angeles battles huge wildfires.',
 ];
 
+function percentDifference(a, b) {
+  return 100 * Math.abs((a - b) / ((a + b) / 2));
+}
+
 export default function AppSidebar() {
   const [loading, setLoading] = useState(false);
   const [crypto, setCrypto] = useState([]);
@@ -26,7 +30,18 @@ export default function AppSidebar() {
       const { result } = await fetchCrypto();
       const assets = await fetchAssets();
 
-      setAssets(assets);
+      setAssets(
+        assets.map(asset => {
+          const coin = result.find(c => c.id === asset.id);
+          return {
+            grow: asset.price < coin.price,
+            growPercent: percentDifference(asset.price, coin.price),
+            totalAmount: asset.amount * coin.price,
+            totalProfit: asset.amount * coin.price - asset.amount * asset.price,
+            ...asset,
+          };
+        })
+      );
       setCrypto(result);
       setLoading(false);
     }
