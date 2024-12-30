aaca4ef620860fd6b9c0d3b9ae2e1324bab94c90
LeonID <lmatskevich79@gmail.com>
2024-12-31 00:37:42 +0300

    feat: add AppLayout.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,11 @@
-import { Layout } from 'antd';
-import AppContent from './components/layout/AppContent';
-import AppFooter from './components/layout/AppFooter';
-import AppHeader from './components/layout/AppHeader';
-import AppSidebar from './components/layout/AppSidebar';
+import AppLayout from './components/layout/AppLayout';
 import { CryptoContextProvider } from './context/crypto-context';
 
 export default function App() {
   return (
     <>
       <CryptoContextProvider>
-        <Layout>
-          <AppHeader />
-          <Layout>
-            <AppSidebar />
-            <AppContent />
-          </Layout>
-          <AppFooter />
-        </Layout>
+        <AppLayout />
       </CryptoContextProvider>
     </>
   );
