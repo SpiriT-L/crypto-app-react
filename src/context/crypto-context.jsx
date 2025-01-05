553b91f181a268f88173514b8a5c92957aa93df1
LeonID <lmatskevich79@gmail.com>
2025-01-05 15:21:32 +0300

    feat: add export use Crypto

--- src/context/crypto-context.jsx
+++ src/context/crypto-context.jsx
@@ -1,5 +1,5 @@
-import { createContext, useEffect, useState } from 'react';
-import { fetchAssets, fetchCrypto } from '../components/layout/api';
+import { createContext, useContext, useEffect, useState } from 'react';
+import { fetchAssets, fetchCrypto } from '../components/layout/api/';
 import { percentDifference } from '../components/utils';
 
 const CryptoContext = createContext({
@@ -45,3 +45,7 @@ export function CryptoContextProvider({ children }) {
 }
 
 export default CryptoContext;
+
+export function useCrypto() {
+  return useContext(CryptoContext);
+}
